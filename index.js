cd5415ad359dcbe5c12c880320ec1cc1c6070559
phuongha2012 <hami201289@him-mail.ch>
2020-09-12 21:33:04 +1200

    created init function

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-const chalk = require('chalk'); // creates large letters with text
-const figlet = require('figlet'); // applies colors and styles
+const program = require('commander');
+const chalk = require('chalk');  // applies colors and styles
+const figlet = require('figlet'); // creates large letters with text
 const clear = require('clear'); // removes current output in the console
 
 const files = require('./lib/files');
 
+program
+    .command('init')
+    .description('Draw app banner')
+    .action(() => {
+        clear();
+        console.log(chalk.magenta(figlet.textSync('git-automate-cli', { horizontalLayout: false })));
+    });
+
+program.parse(process.argv);
+
+if (!program.args.length) {
+    program.help();
+}
 
