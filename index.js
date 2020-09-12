b3d8896e6f91bbcfd0b02f09fd8f5d222d0a4d82
phuongha2012 <hami201289@him-mail.ch>
2020-09-12 21:24:40 +1200

    created file utility methods

---
+++ index.js
@@ -0,0 +1,7 @@
+const chalk = require('chalk'); // creates large letters with text
+const figlet = require('figlet'); // applies colors and styles
+const clear = require('clear'); // removes current output in the console
+
+const files = require('./lib/files');
+
+
